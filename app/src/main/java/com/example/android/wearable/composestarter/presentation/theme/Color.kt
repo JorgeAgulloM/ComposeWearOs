96aac4c372a9971943c675fe766915b4919009bc
Jorge Agullo <agullojorge@gmail.com>
2022-06-08 12:16:59 +0200

    [FEATURE] WIP little changes

--- app/src/main/java/com/example/android/wearable/composestarter/presentation/theme/Color.kt
+++ app/src/main/java/com/example/android/wearable/composestarter/presentation/theme/Color.kt
@@ -24,10 +24,14 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Red400 = Color(0xFFCF6679)
 
+val NttPrimary = Color(0xFF005EFF)
+val NttPrimaryVariant = Color(0xFFFFC400)
+val NttSecondary = Color(0xFF002461)
+
 internal val wearColorPalette: Colors = Colors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = NttPrimary,
+    primaryVariant = NttPrimaryVariant,
+    secondary = NttSecondary,
     secondaryVariant = Teal200,
     error = Red400,
     onPrimary = Color.Black,
