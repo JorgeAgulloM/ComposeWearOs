22f7e237d934a4b5f9e26983b50c404e87a3ac1b
Jorge Agullo <agullojorge@gmail.com>
2022-06-09 16:22:26 +0200

    [FEATURE] WIP added new screen

---
+++ app/src/main/java/com/example/android/wearable/composestarter/presentation/screens/CardsScreen.kt
@@ -0,0 +1,1 @@
+package com.example.android.wearable.composestarter.presentation.screensimport androidx.compose.runtime.Composableimport androidx.navigation.NavController@Composablefun CardsScreen(navController: NavController) {}
